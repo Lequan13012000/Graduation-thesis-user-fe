44367ba06a6e84424e42dac86c79f22213d6e11f
quanlda <quanlda@toprate.io>
2023-02-01 22:14:00 +0700

    init user frontend project

---
+++ src/components/TheContent/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="footer">
+        <div class="footer__content">
+            <img src="../../assets/image/logo.png" alt="">
+        </div>
+        <div class="footer__content">
+            <div class="icon-boder"><i class="fas fa-map-marker-alt"></i></div>
+            <span>Số 90, Ngõ 553 Giải Phóng, Phường Giáp Bát, Quận Hoàng Mai, Tp. Hà Nội</span>
+        </div>
+        <div class="footer__content">
+            <div class="icon-boder"><i class="fas fa-phone-alt"></i></div>
+            <span>0899 189 499</span>
+        </div>
+        <div class="footer__content">
+            <div class="icon-boder"><i class="fas fa-envelope"></i></div>
+            <span>vppanlocviet@gmail.com</span>
+        </div>
+
+    </div>
+</template>
+<style scoped>
+.footer{
+    display: flex;
+    padding: 12px 48px;
+    gap: 24px;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #fff;
+}
+.footer__content{
+    display: flex;
+    gap: 4px;
+    align-items: center;
+}
+.icon-boder{
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #3DAA12;
+    border-radius: 4px;
+}
+</style>
---
+++ src/components/TheContent/TopNavBar.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="top-navbar">
+        <router-link to="/content" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="1000">Trang chủ</router-link>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="1400">Giới thiệu</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="1800">Liên hệ</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="2100">Hỗ trợ</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="2400">Dịch vụ</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="2700">Chính sách</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="3000">Câu hỏi thường gặp</a>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.top-navbar{
+    display: flex;
+    padding: 0 48px;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+    background-color: #3DAA12;
+}
+.top-navbar__item{
+    height: 40px;
+    min-width: 100px;
+    padding: 0 12px;
+    display: block;
+    line-height: 40px;
+    text-align: center;
+    color: #fff;
+    font-size: 20px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    font-weight: 600;
+    transition: all linear 0.5s;
+}
+.top-navbar__item:hover{
+    color: #3DAA12;
+    background: #fff;
+    border: 1px solid #3DAA12;
+}
+</style>
