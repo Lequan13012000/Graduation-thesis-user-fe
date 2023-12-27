cf1de3f5228892647c2f04f1c5150a1f56f79a56
Le Quan <quanchinh13012000@gmail.com>
2023-12-27 23:01:36 +0700

    fix: deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/Graduation-thesis-user-fe/' // Thay tên repository của các bạn vào đây nhé
-      : '/'
+    publicPath:  '/Graduation-thesis-user-fe/' // Thay tên repository của các bạn vào đây nhé
   }
-  
+   
